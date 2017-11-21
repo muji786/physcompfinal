5abe4ad7c90f46dc815c9798e699c49a0fd8788f
Ahmad Arshad <muji786@users.noreply.github.com>
2017-11-20 19:46:24 -0500

    Update sketch.js

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,14 +1,3 @@
-//Serial Variables
-let serial; // variable to hold an instance of the serialport library
-let portName = '/dev/cu.usbmodem1421';
-let inData; // for incoming serial data
-let sensor1;
-let sensor2;
-let sensor3;
-let sensor4;
-
-
-//Sketch Variables
 let carrier; // this is the oscillator we will hear
 let modulator; // this oscillator will modulate the frequency of the carrier
 let filter, filterfreq, filterres;
@@ -16,14 +5,13 @@ let analyzer; // we'll use this visualize the waveform
 let delay;
 let distortion;
 let env;
-let oscType;
+let oscType = "sine"; // default value is sine
 
 //array to be used for assigning the base oscillator frequency in the C minor scale
 let freqArray = [261.626, 293.665, 311.127, 349.228, 391.995, 415.305, 466.164];
 
 //array to be used for assigning oscillator type
 let oscArray = ['sine', 'square', 'sawtooth', 'triangle'];
-
 
 // the carrier frequency pre-modulation
 let carrierBaseFreq;
@@ -39,45 +27,22 @@ function setup() {
     let cnv = createCanvas(800, 400);
     noFill();
 
-    
-    //********************************************SERIAL SETUP********************************************
-    //********************************************SERIAL SETUP********************************************
-    //********************************************SERIAL SETUP********************************************
-    //********************************************SERIAL SETUP********************************************
-    serial = new p5.SerialPort(); // make a new instance of the serialport library
-    serial.on('list', printList); // set a callback function for the serialport list event
-    serial.on('connected', serverConnected); // callback for connecting to the server
-    serial.on('open', portOpen); // callback for the port opening
-    serial.on('data', serialEvent); // callback for when new data arrives
-    serial.on('error', serialError); // callback for errors
-    serial.on('close', portClose); // callback for the port closing
-
-    serial.list(); // list the serial ports
-    serial.open(portName); // open a serial port
-    //****************************************************************************************************
-    //****************************************************************************************************
-    //****************************************************************************************************
-    //****************************************************************************************************
-
-    let r = round(random(0, freqArray.length));
-    carrierBaseFreq = freqArray[r];
-
+    carrierBaseFreq = random(freqArray);
     oscType = random(oscArray);
 
     carrier = new p5.Oscillator(random(oscArray));
     carrier.amp(0); // set amplitude
 
-    
     //****************************************************************************************************
     //THE BELOW ENVELOPE IS NOT YET WORKING.  THIS SHOULD CREATE A SMOOTH START TO EACH NOTE DUE TO 
     //THE LONG ATTACK VALUE OF 0.8
     //****************************************************************************************************
-    
+
     env = new p5.Env();
     // set attackTime, decayTime, sustainRatio, releaseTime
-    env.setADSR(0.8, 0.7, 0.2, 0.8);
+    env.setADSR(1, 1, 0.2, 3);
     // set attackLevel, releaseLevel
-    env.setRange(1.0, 0);
+    env.setRange(1, 0);
 
     //****************************************************************************************************
     //THE ABOVE ENVELOPE IS NOT YET WORKING.  THIS SHOULD CREATE A SMOOTH START TO EACH NOTE DUE TO 
@@ -88,18 +53,14 @@ function setup() {
     carrier.start(); // start oscillating
 
     distortion = new p5.Distortion();
-    console.log(oscType);
-
 
     filter = new p5.LowPass();
     carrier.disconnect();
 
-    delaySounds();
-
+    // delaySounds();
 
     carrier.connect(filter);
     carrier.connect(distortion);
-    //carrier.connect(env);
     //distortion.drywet(.1);
 
     // try changing the type to 'square', 'sine' or 'triangle'
@@ -121,24 +82,24 @@ function draw() {
     background(30);
     display();
 
-    text('Carrier Oscillator Type: ' + oscType, width/2, 40);
+    //    text('Carrier Oscillator Type: ' + oscType, width / 2, 40);
 
 
 }
 
-function display(){
-  // map mouseY to modulator freq between a maximum and minimum frequency
-  let modFreq = map(mouseY, height, 0, modMinFreq, modMaxFreq);
-  modulator.freq(modFreq);
-  filterfreq = map(mouseX, 0, width, 10, 22050);
-  filterres = map(mouseY, 0, height, 15, 5);
-  filter.freq(filterfreq);
-  filter.res(filterres);
+function display() {
+    // map mouseY to modulator freq between a maximum and minimum frequency
+    let modFreq = map(mouseY, height, 0, modMinFreq, modMaxFreq);
+    modulator.freq(modFreq);
+    filterfreq = map(mouseX, 0, width, 10, 22050);
+    filterres = map(mouseY, 0, height, 15, 5);
+    filter.freq(filterfreq);
+    filter.res(filterres);
     //console.log(filterfreq);
 
     // change the amplitude of the modulator
     // negative amp reverses the sawtooth waveform, and sounds percussive
-    
+
     let modDepth = map(mouseX, mouseY, width, modMinDepth, modMaxDepth);
     modulator.amp(modDepth);
 
@@ -162,109 +123,38 @@ function display(){
     text('Modulator Frequency: ' + modFreq.toFixed(3) + ' Hz', 20, 20);
     text('Modulator Amplitude (Modulation Depth): ' + modDepth.toFixed(3), 20, 40);
     text('Carrier Frequency (pre-modulation): ' + carrierBaseFreq + ' Hz', width / 2, 20);
-    //console.log(carrierBaseFreq);
+    text('Carrier Oscillator Type: ' + oscType, width / 2, 40);
+    console.log(oscType);
 }
 
-function delaySounds(){
+function delaySounds() {
 
     delay = new p5.Delay();
-
     // .12, .7. 2300 as default
     // source, delayTime, feedback, filter frequency
     delay.process(carrier, .12, .7, 20000);
-    delay.setType('pingPong'); 
+    delay.setType('pingPong');
 
     //delay.filter(filterFreq, filterRes);
     let delTime = map(mouseY, 0, width, .5, .01);
     delTime = constrain(delTime, .01, .5);
     delay.delayTime(delTime);
-  // delay.process(carrier, 0.22, feedback, 230000);
+    // delay.process(carrier, 0.22, feedback, 230000);
 
 }
 
 // helper function to toggle sound
 function toggleAudio(cnv) {
-    cnv.mouseOver(function () {
-        carrier.amp(0.2, 0.01);
+    cnv.mouseClicked(function () {
+        //carrier.amp(0.2, 0.01);
+        env.play(carrier);
     });
-    cnv.touchStarted(function () {
-        carrier.amp(0.2, 0.01);
-    });
-    cnv.mouseOut(function () {
-        carrier.amp(0.0, 1.0);
-    });
+    //    cnv.touchStarted(function () {
+    //        // carrier.amp(0.2, 0.01);
+    //    });
+    //    cnv.mouseOut(function () {
+    //        //carrier.amp(0.0, 1.0);
+    //        env.play(carrier);
+    //
+    //    });
 }
-
-
-
-//**************************************SERIAL FUNCTIONS*******************************************
-//**************************************SERIAL FUNCTIONS*******************************************
-//**************************************SERIAL FUNCTIONS*******************************************
-//**************************************SERIAL FUNCTIONS*******************************************
-
-// get the list of ports:
-function printList(portList) {
-    // portList is an array of serial port names
-    for (var i = 0; i < portList.length; i++) {
-        // Display the list the console:
-        console.log(i + " " + portList[i]);
-    }
-}
-
-function serverConnected() {
-    console.log('connected to server.');
-}
-
-function portOpen() {
-    console.log('the serial port opened.')
-}
-
-function serialEvent() {
-    // read a string from the serial port:
-    var inString = serial.readStringUntil('\r\n');
-    // check to see that there's actually a string there:
-    if (inString.length > 0) {
-        var sensors = split(inString, ',');
-        sensor1 = sensors[1];
-        sensor2 = sensors[0];
-        sensor3 = sensors[2];
-        sensor4 = sensors[3];
-
-        // if (sensor1 >= 25) {
-        //     var m = map(sensor1, 25, 1023, 0.1, 1.0);
-
-        //     }
-        // }
-        // if (sensor2 >= 25) {
-        //     var m = map(sensor2, 25, 1023, 0.1, 1.0);
-
-
-        //     }
-        // }
-        // if (sensor3 >= 25) {
-        //     var m = map(sensor3, 25, 1023, 0.1, 1.0);
-
-        //     }
-        // }
-        if (sensor4 >= 100) {
-            var m = map(sensor4, 25, 1023, 0.1, 1.0);     
-
-            //carrier.amp(0.2, 0.01);
-            env.setADSR(0.8, 0.7, 0.2, 0.8);
-
-
-            console.log("playing Cello at volume : " + m);
-
-        }
-    }
-}
-
-
-function serialError(err) {
-    console.log('Something went wrong with the serial port. ' + err);
-}
-
-function portClose() {
-    console.log('The serial port closed.');
-}
-
