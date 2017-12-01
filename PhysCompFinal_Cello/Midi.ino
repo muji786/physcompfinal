34bb1c11c9f34998c50fe2628a436e5f199fe080
Ahmad Arshad <muji786@users.noreply.github.com>
2017-12-01 18:28:21 -0500

    update

--- PhysCompFinal_Cello/Midi.ino
+++ PhysCompFinal_Cello/Midi.ino
@@ -46,6 +46,8 @@ void setup() {
 
 void loop() {
   // Play notes from F3 (53) to A6 (93):
+  MIDI.sendControlChange(07,127,1);
+  delay(1000);
   for (int note = 53; note <= 93; note ++) {
     // Send a NoteOn message, wait a second, turn the note off
     MIDI.sendNoteOn(note, velocity, channel);  
