80e99709dbfed40e06ea43d2e524359cdc3d7c21
Ahmad Arshad <muji786@users.noreply.github.com>
2017-12-05 15:43:40 -0500

    radical change

--- Midi/Midi.ino
+++ Midi/Midi.ino
@@ -1,20 +1,3 @@
-/*
-  MIDI note player
-  Author: Brendan Vavra
-  This sketch shows how to use the serial transmit pin (pin 1) to send MIDI note data.
-  If this circuit is connected to a MIDI synth, it will play
-  the notes from F3 (53) to A6 (93) in sequence.
-  This tutorial has been modified to use the Arduino MIDI library:
-  http://playground.arduino.cc/Main/MIDILibrary
-  Original code is in the public domain:
-  http://www.arduino.cc/en/Tutorial/Midi
-  If using a serial-to-MIDI converter, no circuit components are necessary.
-  If sending MIDI over a MIDI cable, the circuit is as follows:
-  - digital in 1 connected to MIDI jack pin 5
-  - MIDI jack pin 2 connected to ground
-  - MIDI jack pin 4 connected to +5V through 220-ohm resistor
-  Attach a MIDI cable to the jack, then to a MIDI synth (or PC), and play music.
-*/
 #include <MIDI.h>
 #include <midi_Defs.h>
 #include <midi_Message.h>
@@ -22,16 +5,7 @@
 #include <midi_Settings.h>
 struct MySettings : public midi::DefaultSettings
 {
-  //By default, the Arduino MIDI Library tries to be smart by
-  //excluding the CC byte if it doesn't change (to save bandwidth).
-  //This is a problem when starting up Serial<->MIDI software
-  //after starting up the Arduino because we miss the first CC byte.
-  //Setting UseRunningStatus to false removes this "feature."
-  //See https://github.com/projectgus/hairless-midiserial/issues/16 for details.
   static const bool UseRunningStatus = false;
-  // Set MIDI baud rate. MIDI has a default baud rate of 31250,
-  // but we're setting our baud rate higher in order to
-  // properly decode and read outgoing MIDI data on the computer.
   static const long BaudRate = 115200;
 };
 
@@ -39,27 +13,57 @@ MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial, MIDI, MySettings);
 
 const int velocity = 127; //Max Velocity (range is 0-127)
 const int channel = 1; //MIDI Channel 1 (out of 16)
+const int potSense1 = A0;
+const int potSense2 = A1;
+const int potSense3 = A2;
+const int potSense4 = A3;
+const int potSense5 = A4;
+int lastPot1Value = 0;
+int lastPot2Value = 0;
+int lastPot3Value = 0;
+int lastPot4Value = 0;
+int lastPot5Value = 0;
 
 void setup() {
   MIDI.begin();
 }
 
 void loop() {
-  int pot = analogRead(A4) / 8; // read the input pin and divide by 8 to get between 0-127 for midi
-  int lastPot = 0;
+  int newPot1Value = analogRead(potSense1) / 8; // read the input pin and divide by 8 to get between 0-127 for midi
+  
+  // int val = map(pot, 0, 1023, 0, 127);
+  
+  if (newPot1Value != lastPot1Value) {
+    MIDI.sendControlChange(01, newPot1Value, 1);
+    lastPot1Value = newPot1Value;
+  }
 
-  if (lastPot != pot) {
-    MIDI.sendControlChange(01, pot, 1);
-    lastPot = pot;
-    delay(1000);
+  int newPot2Value = analogRead(potSense2) / 8; // read the input pin and divide by 8 to get between 0-127 for midi
+
+  if (newPot2Value != lastPot2Value) {
+    MIDI.sendControlChange(02, newPot2Value, 1);
+    lastPot2Value = newPot2Value;
+  }
+
+  int newPot3Value = analogRead(potSense3) / 8; // read the input pin and divide by 8 to get between 0-127 for midi
+  
+  if (newPot3Value != lastPot3Value) {
+    MIDI.sendControlChange(03, newPot3Value, 1);
+    lastPot3Value = newPot3Value;
+  }
+
+   int newPot4Value = analogRead(potSense4) / 8; // read the input pin and divide by 8 to get between 0-127 for midi
+
+  if (newPot4Value != lastPot4Value) {
+    MIDI.sendControlChange(04, newPot4Value, 1);
+    lastPot4Value = newPot4Value;
+  }
+
+    int newPot5Value = analogRead(potSense5) / 8; // read the input pin and divide by 8 to get between 0-127 for midi
+
+  if (newPot5Value != lastPot5Value) {
+    MIDI.sendControlChange(05, newPot5Value, 1);
+    lastPot5Value = newPot5Value;
   }
   
-  int sensor = analogRead(A3)/8; // read the input pin
-  int pSensor = 0;
-  if (pSensor != sensor) {
-    MIDI.sendControlChange(02, sensor, 1);
-    pSensor = sensor;
-    delay(1000);
-
-  }
 }
