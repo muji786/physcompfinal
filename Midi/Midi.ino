b9d608c8c5cfe487c8cf756101673df1f9048077
jps723 <31663418+jps723@users.noreply.github.com>
2018-01-17 23:41:55 -0500

    Added comments and cleaned up for documentation
    Added comments and cleaned up for documentation

--- Midi/Midi.ino
+++ Midi/Midi.ino
@@ -6,19 +6,21 @@
 struct MySettings : public midi::DefaultSettings
 {
   static const bool UseRunningStatus = false;
-  static const long BaudRate = 115200;
+  static const long BaudRate = 115200; //Baudrate for Hairless
 };
 
+//Formatted as (Serial Type (HW of SW), name of arduino serial, name of port, custom settings)
 MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial, MIDI, MySettings);
 
-//const int velocity = 127; //Max Velocity (range is 0-127)
-//const int channel = 1; //MIDI Channel 1 (out of 16)
+//declaring pots
 const int potSense0 = A0;
 const int potSense1 = A1;
 const int potSense2 = A2;
 const int potSense3 = A3;
 const int potSense4 = A4;
 const int potSense5 = A5;
+
+//declaring previous value variables
 int lastPot0Value = 0;
 int lastPot1Value = 0;
 int lastPot2Value = 0;
@@ -27,48 +29,55 @@ int lastPot4Value = 0;
 int lastPot5Value = 0;
 
 void setup() {
+  //Start listening for MIDI in on the serial port.  Used in place of Serial.begin()
   MIDI.begin();
 }
 
 void loop() {
-  int newPot0Value = analogRead(potSense0) / 8; // read the input pin and divide by 8 to get between 0-127 for midi
-    
+  //Divide sensor reading by 8 to get between 0-127 -- MIDI Standard
+  int newPot0Value = analogRead(potSense0) / 8;  
   if (newPot0Value != lastPot0Value) {
+    //Send MIDI CC (Control Number (0-127), pot reading, MIDI Channel (1-16)) 
     MIDI.sendControlChange(01, newPot0Value, 1);
     lastPot0Value = newPot0Value;
   }
 
-  int newPot1Value = analogRead(potSense1) / 8; // read the input pin and divide by 8 to get between 0-127 for midi
+  int newPot1Value = analogRead(potSense1) / 8; 
     
   if (newPot1Value != lastPot1Value) {
+    //Send MIDI CC (Control Number (0-127), pot reading, MIDI Channel (1-16)) 
     MIDI.sendControlChange(02, newPot1Value, 1);
     lastPot1Value = newPot1Value;
   }
 
-  int newPot2Value = analogRead(potSense2) / 8; // read the input pin and divide by 8 to get between 0-127 for midi
+  int newPot2Value = analogRead(potSense2) / 8; 
 
   if (newPot2Value != lastPot2Value) {
+    //Send MIDI CC (Control Number (0-127), pot reading, MIDI Channel (1-16)) 
     MIDI.sendControlChange(03, newPot2Value, 1);
     lastPot2Value = newPot2Value;
   }
 
-  int newPot3Value = analogRead(potSense3) / 8; // read the input pin and divide by 8 to get between 0-127 for midi
+  int newPot3Value = analogRead(potSense3) / 8; 
   
   if (newPot3Value != lastPot3Value) {
+    //Send MIDI CC (Control Number (0-127), pot reading, MIDI Channel (1-16)) 
     MIDI.sendControlChange(04, newPot3Value, 1);
     lastPot3Value = newPot3Value;
   }
 
-   int newPot4Value = analogRead(potSense4) / 8; // read the input pin and divide by 8 to get between 0-127 for midi
+   int newPot4Value = analogRead(potSense4) / 8; 
 
   if (newPot4Value != lastPot4Value) {
+    //Send MIDI CC (Control Number (0-127), pot reading, MIDI Channel (1-16)) 
     MIDI.sendControlChange(05, newPot4Value, 1);
     lastPot4Value = newPot4Value;
   }
 
-    int newPot5Value = analogRead(potSense5) / 8; // read the input pin and divide by 8 to get between 0-127 for midi
+    int newPot5Value = analogRead(potSense5) / 8; 
 
   if (newPot5Value != lastPot5Value) {
+    //Send MIDI CC (Control Number (0-127), pot reading, MIDI Channel (1-16)) 
     MIDI.sendControlChange(06, newPot5Value, 1);
     lastPot5Value = newPot5Value;
   }
